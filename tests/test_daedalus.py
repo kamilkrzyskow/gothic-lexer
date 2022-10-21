b0b61b552e2fa0a0b84819cb6280e52b6e26d229
Kamil Krzyśków <kamilzary@gmail.com>
2022-10-21 22:53:12 +0200

    add new test for var block

--- tests/test_daedalus.py
+++ tests/test_daedalus.py
@@ -4,15 +4,17 @@ Test suite for the Daedalus lexer
 import os
 import unittest
 
-from gothic_lexer import DaedalusLexer
-from pygments import lexers
-
 import general_tokens
 import misc_tokens
+import var_tokens
+from pygments import lexers
+
+from gothic_lexer import DaedalusLexer
 
 TESTS_DIR_PATH = os.path.abspath(os.path.dirname(__file__))
 GENERAL_D_PATH = os.path.join(TESTS_DIR_PATH, "general.d")
 MISC_D_PATH = os.path.join(TESTS_DIR_PATH, "misc.d")
+VAR_D_PATH = os.path.join(TESTS_DIR_PATH, "var.d")
 
 
 class DaedalusTest(unittest.TestCase):
@@ -54,6 +56,19 @@ class DaedalusTest(unittest.TestCase):
         for token in tokens:
             if "Whitespace" not in str(token):
                 self.assertEqual(token, next(correct))
+
+    def test_var_tokens(self) -> None:
+        """
+        Test that the lexer tokenizes the `var.d` file correctly
+        """
+        with open(VAR_D_PATH, encoding="utf8") as file:
+            source: str = file.read()
+
+        tokens = DaedalusLexer().get_tokens(source)
+        correct = iter(var_tokens.CORRECT_TOKENS)
+
+        for token in tokens:
+            self.assertEqual(token, next(correct))
 
 
 if __name__ == "__main__":
---
+++ tests/var.d
@@ -0,0 +1,6 @@
+func string Str_Format( var string format, ... ) {};
+func string Str_GetLocalizedString( var string russian, var string english, var string german, var string polish ) {};
+func string Str_GetLocalizedStringEx( var string russian, var string english, var string german, var string polish, var string romanian, var string italian, var string czech, var string spanish ) {};
+func string Str_UTF8_to_ANSI( var string utf8, var int codePage ) {};
+func int Str_GetCurrentCP() {};
+func int Str_GetLength( var int str ) {};
